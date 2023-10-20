226ea7601942e087dcd0a114c34cf44bbac7746c
Dulgisng <115183145+Dulgisng@users.noreply.github.com>
2023-10-20 05:04:51 +0000

    Adding a SQL Database file for table reservation

---
+++ data.sql
@@ -0,0 +1,9 @@
+CREATE TABLE reservations (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    time TIME NOT NULL,
+    people INT NOT NULL,
+    requests TEXT
+);
