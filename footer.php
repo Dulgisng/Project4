f52271e463c3963bbe04e5591426d426578f622c
Dulgisng <115183145+Dulgisng@users.noreply.github.com>
2023-10-20 02:23:15 +0000

    Creating css/img folder and adding css file

---
+++ footer.php
@@ -0,0 +1,28 @@
+    <!-- Start of footer section -->
+
+    <footer id="footer" class="footer-section">
+        <div class="footer-content">
+
+            <!-- Social media links section -->
+
+            <div class="footer-social">
+                <a href="index.php" class="social-link"><i class="fa fa-facebook"></i></a>
+                <a href="index.php" class="social-link"><i class="fa fa-twitter"></i></a>
+                <a href="index.php" class="social-link"><i class="fa fa-pinterest"></i></a>
+            </div>
+
+            <!-- Contact info and copyright section -->
+
+            <div class="footer-info">
+                <p><i class="fa fa-phone"></i> +353 (01) 123 4567</p>
+                <p><i class="fa fa-envelope-o"></i> info@itali.com</p>
+                <p>Site by Dulguun Sandagsuren &copy; 2023</p>
+            </div>
+        </div>
+    </footer>
+
+    <!-- End of footer section -->
+
+</body>
+
+</html>
